6b257d401d8c9907c402baea90ebf7b94eb8780f
anjnakS <anjnakhanna69@gmail.com>
2022-02-19 19:39:05 +0000

    Added docker files

--- src/authConfig.js
+++ src/authConfig.js
@@ -12,9 +12,9 @@ import { LogLevel } from "@azure/msal-browser";
  */
 export const msalConfig = {
     auth: {
-        clientId: "Enter_the_Application_Id_Here",
-        authority: "Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here",
-        redirectUri: "Enter_the_Redirect_Uri_Here"
+        clientId: "ef7f073f-dd38-48fc-9438-4ea36e7e847cß",
+        authority: "https://login.microsoftonline.com/8d09f28d-2b54-4761-98f1-de38762cd939",
+        redirectUri: "http://localhost:3000"
     },
     cache: {
         cacheLocation: "sessionStorage", // This configures where your cache will be stored
@@ -60,5 +60,5 @@ export const loginRequest = {
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
  */
 export const graphConfig = {
-    graphMeEndpoint: "Enter_the_Graph_Endpoint_Herev1.0/me"
+    graphMeEndpoint: "https://graph.microsoft.com/me"
 };
